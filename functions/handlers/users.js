9011333d8aa211fa8fa3a54812207aa77b625970
James Porter <43405677+JamesP1996@users.noreply.github.com>
2020-11-18 18:58:16 +0000

    Created Profile Image Upload

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -1,12 +1,14 @@
-const { db } = require("../utilities/admin");
+const { db, admin } = require("../utilities/admin");
 
 const config = require("../utilities/config");
 
-const firebase = require('firebase');
+const firebase = require("firebase");
 firebase.initializeApp(config);
 
-
-const {validateSignupData , validateLoginData} = require('../utilities/validators');
+const {
+  validateSignupData,
+  validateLoginData,
+} = require("../utilities/validators");
 
 exports.signup = (req, res) => {
   const newUser = {
@@ -16,9 +18,11 @@ exports.signup = (req, res) => {
     handle: req.body.handle,
   };
 
-  const {valid,errors} = validateSignupData(newUser); 
+  const { valid, errors } = validateSignupData(newUser);
 
-  if(!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors);
+
+  const noImg = "no-img.png";
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
@@ -42,6 +46,7 @@ exports.signup = (req, res) => {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
         userID: userId,
       };
       return db.doc(`/users/${newUser.handle}`).set(userCredentials);
@@ -64,9 +69,9 @@ exports.login = (req, res) => {
     password: req.body.password,
   };
 
-  const {valid,errors} = validateLoginData(user); 
+  const { valid, errors } = validateLoginData(user);
 
-  if(!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors);
 
   firebase
     .auth()
@@ -88,4 +93,56 @@ exports.login = (req, res) => {
           .json({ general: "Wrong credentials, please try again" });
       } else return res.status(500).json({ error: err.code });
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+  if(mimetype !== 'image/jpeg' && mimetype !== 'image/png'){
+    return res.status(400).json({error: "Wrong File Type Uploaded!\nOnly Accepts PNG/JPEG"})
+  }
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(fieldname);
+    console.log(filename);
+    console.log(mimetype);
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    const imageFileName = `${Math.round(
+      Math.random() * 1000000
+    )}.${imageExtension}`;
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc("/users/${req.user.handle").update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+  busboy.end(req.rawBody);
 };
--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,10 @@ const functions = require("firebase-functions");
 
 const Authorization = require("./utilities/FBAuth");
 
-const { signup, login } = require("./handlers/users");
+const { signup, login ,uploadImage} = require("./handlers/users");
 const {getAllNotes} = require("../functions/handlers/notes");
 const {postNewNote } = require("./handlers/notes");
+const FBAuth = require("./utilities/FBAuth");
 
 const app = require("express")();
 
@@ -17,6 +18,7 @@ app.post("/notes",Authorization,postNewNote);
 // --USER ROUTES--
 app.post("/signup", signup);
 app.post("/login", login);
+app.post('/user/image',FBAuth,uploadImage);
 
 // Exporting the Express App that contains all the routes needed
 // Uses the Firebase Functions Region of Europe Currently
