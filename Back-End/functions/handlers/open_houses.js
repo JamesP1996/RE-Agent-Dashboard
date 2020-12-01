f16debe1da635d525dd9182d6f260ecc5f9aaf0b
James Porter <43405677+JamesP1996@users.noreply.github.com>
2020-12-01 00:11:07 +0000

    Fixed OpenHouses Function UploadImage

--- Back-End/functions/handlers/open_houses.js
+++ Back-End/functions/handlers/open_houses.js
@@ -1,8 +1,7 @@
 const { db, admin } = require("../utilities/admin");
 
 const config = require("../utilities/config");
-const { v4: uuid } = require('uuid');
-
+const { v4: uuid } = require("uuid");
 
 exports.getAllHouses = (req, res) => {
   db.collection("open_houses")
@@ -145,66 +144,62 @@ exports.updateHouse = (req, res) => {
         });
     }
   });
+};
 
-  exports.uploadHouseImage = (req, res) => {
-    const BusBoy = require("busboy");
-    const path = require("path");
-    const os = require("os");
-    const fs = require("fs");
+exports.uploadHouseImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
 
-    const busboy = new BusBoy({ headers: req.headers });
+  const busboy = new BusBoy({ headers: req.headers });
 
-    let rand = `${Math.round(Math.random() * 1000000)}`;
-    let imageFileName;
-    let imageToBeUploaded = {};
-    let generatedToken = uuid();
+  let rand = `${Math.round(Math.random() * 1000000)}`;
+  let imageFileName;
+  let imageToBeUploaded = {};
+  let generatedToken = uuid();
 
-    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
-      console.log(filename, mimetype);
-      if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
-        return res
-          .status(400)
-          .json({ error: "Wrong File Type Uploaded!\nOnly Accepts PNG/JPEG" });
-      }
-      const imageExtension = filename.split(".")[
-        filename.split(".").length - 1
-      ];
+  busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+    console.log(filename, mimetype);
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res
+        .status(400)
+        .json({ error: "Wrong File Type Uploaded!\nOnly Accepts PNG/JPEG" });
+    }
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
 
-      imageFileName = `${rand}.${imageExtension}`;
+    imageFileName = `${rand}.${imageExtension}`;
 
-      const filepath = path.join(os.tmpdir(), imageFileName);
-      imageToBeUploaded = { filepath, mimetype };
-      file.pipe(fs.createWriteStream(filepath));
-    });
-    busboy.on("finish", () => {
-      admin
-        .storage()
-        .bucket()
-        .upload(imageToBeUploaded.filepath, {
-          resumable: false,
+    const filepath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filepath, mimetype };
+    file.pipe(fs.createWriteStream(filepath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filepath, {
+        resumable: false,
+        metadata: {
           metadata: {
-            metadata: {
-              contentType: imageToBeUploaded.mimetype,
-              firebaseStorageDownloadTokens: generatedToken,
-            },
+            contentType: imageToBeUploaded.mimetype,
+            firebaseStorageDownloadTokens: generatedToken,
           },
-        })
-        .then(() => {
-          const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
-          return db
-            .doc(`/open_houses/${req.body.houseID}`)
-            .update({ imageUrl });
-        })
-        .then(() => {
-          return res.json({ message: "Image uploaded successfully" });
-        })
-        .catch((err) => {
-          console.error(err);
-          return res
-            .status(500)
-            .json({ error: `something went wrong: ${err.code}` });
-        });
-    });
-    busboy.end(req.rawBody);
-  };
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media&token=${generatedToken}`;
+        return db.doc(`/open_houses/${req.body.houseID}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: `something went wrong: ${err.code}` });
+      });
+  });
+  busboy.end(req.rawBody);
 };
